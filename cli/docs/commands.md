07046798a0a48791effc3ad7f53a46b8a35f9023
santiagoecheverri94 <santiagoecheverri94@gmail.com>
2023-06-21 11:34:29 -0500

    feat(oclif): change cli tool

--- cli/docs/commands.md
+++
@@ -1,3 +0,0 @@
-# Command Reference for hedgeware
-
-TODO: Add your command reference here
---
+++ trading/options/liquidityArb.ts
@@ -0,0 +1,1 @@
+// Helps preserve options premium written by arbitraging the liquidity of the underlying equity.
