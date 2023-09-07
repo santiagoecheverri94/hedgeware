739f6d22bc18a55358df2190049c0520579dcc5c
santiagoecheverri94 <santiagoecheverri94@gmail.com>
2023-09-06 19:01:21 -0500

    removed faulty notes

--- src/trading/strategies/liquidity-arb.ts
+++ src/trading/strategies/liquidity-arb.ts
@@ -1,13 +1,4 @@
-/**
- * Algo Notes:
- * 0. (not sure if this is needed) Always keep track of last ask price?
- * 1. Check what is your current ask price
- * 2. Check if you have the positions correctly sized given current ask price
- * 3. If yes, do nothing; if no, buy position to correctly size
- * 4. Repeat for bid price (sell of step 3)
- */
-
-// import {IBKRClient} from '../brokerage-clients/IBKR/ibkr-client'
+import {IBKRClient} from '../brokerage-clients/IBKR/client';
 
 export function startLiquidityArb(): void {
   // const brokerageClient = new IBKRClient()
