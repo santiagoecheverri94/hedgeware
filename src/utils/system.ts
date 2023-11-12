6e28bbf9cf2b92500e03582cf3e9ca93e9c71bb9
santiagoecheverri94 <santiagoecheverri94@gmail.com>
2023-11-12 12:30:28 -0600

    still debug interrupt

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -6,34 +6,15 @@ export function stopSystem(errorMsg: string): void {
 }
 
 export function onUserInterrupt(callback: () => void): void {
-  // const USER_INTERRUPT = 'SIGINT';
-
-  // if (process.platform === 'win32') {
-  //   const terminalInput = createInterface({
-  //     input: process.stdin,
-  //     output: process.stdout,
-  //   });
-
-  //   terminalInput.on(USER_INTERRUPT, () => {
-  //     process.emit(USER_INTERRUPT, USER_INTERRUPT);
-  //   });
-  // }
-
-  // process.on(USER_INTERRUPT, callback);
-
-  if (process.platform === "win32") {
-    var rl = require("readline").createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
+  // process.on('SIGINT', function() {
+  //   process.exit(0);
+  // });
   
-    rl.on("SIGINT", function () {
-      process.emit("SIGINT" as any);
-    });
-  }
+  // process.on('SIGTERM', function() {
+  //   process.exit(0);
+  // });
   
-  process.on("SIGINT", function () {
-    //graceful shutdown
-    process.exit();
-  });
+  // process.on('exit', function() {
+  //   process.stdout.write("Bye");
+  // });
 }
